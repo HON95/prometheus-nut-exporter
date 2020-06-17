535b89d04e6e375cd0b0599246e6eaf035ff1f5d
Håvard O. Nordstrand <haavard@haavard.tech>
2020-06-17 06:16:05 +0200

    Add NUT client stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::env;
+use std::io::Error;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 
 use chrono::Local;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::server::conn::AddrStream;
+use lazy_static::lazy_static;
+use regex::Regex;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
 use url::form_urlencoded;
 
 #[derive(Debug, Clone)]
@@ -21,6 +26,15 @@ impl Config {
     const DEFAULT_HTTP_PATH: &'static str = "/nut";
     const DEFAULT_LOG_REQUESTS_CONSOLE: bool = false;
 }
+
+#[derive(Debug, Clone)]
+struct UpsVar {
+    ups: String,
+    variable: String,
+    value: String,
+}
+
+const UPS_DESCRIPTION_PSEUDOVAR: &'static str = "description";
 
 /*
  * TODO
@@ -64,6 +78,10 @@ fn read_config() -> Config {
     config
 }
 
+/*
+ * Web server stuff
+ */
+
 async fn run_server(config: Config) {
     let endpoint = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), config.http_port);
     let service_maker = make_service_fn(move |conn:  &AddrStream| {
@@ -98,7 +116,7 @@ async fn entrypoint(config: Config, request: Request<Body>, remote_addr: SocketA
         }
     } else if path == metrics_path {
         if is_method_get {
-            response_res = endpoint_metrics(&config, &query_args);
+            response_res = endpoint_metrics(&config, &query_args).await;
         } else {
             response_res = endpoint_method_not_allowed();
         }
@@ -124,16 +142,21 @@ fn endpoint_home(config: &Config) -> Result<Response<Body>, Infallible> {
     Ok(response)
 }
 
-fn endpoint_metrics(config: &Config, query_args: &HashMap<String, String>) -> Result<Response<Body>, Infallible> {
+async fn endpoint_metrics(config: &Config, query_args: &HashMap<String, String>) -> Result<Response<Body>, Infallible> {
     let target = query_args.get("target").map_or("", |s| s.as_str());
 
     let mut content = String::new();
     let mut status = StatusCode::OK;
 
     if target.len() > 0 {
-        // TODO collect and format NUT metrics
-        // Status 503 if failed to connect
         content.push_str(&format!("Metrics!\n\nTarget: {}\n", target));
+        let result_res = scrape_nut(target).await;
+        if let Ok(result) = result_res {
+            content.push_str(&result);
+        } else if let Err(error) = result_res {
+            content.push_str(&error);
+            status = StatusCode::SERVICE_UNAVAILABLE;
+        }
     } else {
         content.push_str(&format!("Missing target.\n\nUsage: {}?target=<target>\n", config.http_path));
         status = StatusCode::BAD_REQUEST;
@@ -165,4 +188,102 @@ fn log_request(config: &Config, request: &Request<Body>, remote_addr: &SocketAdd
         println!("{time} {client} {method} \"{path}\" {status}",
             time=local_time, client=remote_addr, method=request.method(), path=request.uri().path(), status=status);
     }
+}
+
+/*
+ * NUT client stuff
+ */
+
+async fn scrape_nut(target: &str) -> Result<String, String> {
+    // TODO connect, query UPSes, query vars for each UPS
+    // TODO transform vars to pretty metrics with HELP, TYPE, UNIT
+    // TODO build OpenMetrics content
+
+    let raw_stream_res = TcpStream::connect(target).await;
+    if let Err(err) = raw_stream_res {
+        return Err("Failed to connect to target.".to_owned());
+    }
+    let mut stream = BufReader::new(raw_stream_res.unwrap());
+
+    let vars_res = scrape_nut_vars(&mut stream).await;
+    if let Err(err) = vars_res {
+        return Err("Failed to communicate with target.".to_owned());
+    }
+    let vars = vars_res.unwrap();
+
+    let content = build_openmetrics_content(vars);
+
+    Ok(content)
+}
+
+async fn scrape_nut_vars(stream: &mut BufReader<TcpStream>) -> Result<Vec<UpsVar>, Error> {
+    const RAW_UPS_PATTERN: &str = r#"^UPS\s+(?P<ups>[\S]+)\s+"(?P<desc>[^"]*)"$"#;
+    const RAW_VAR_PATTERN: &str = r#"^VAR\s+(?P<ups>[\S]+)\s+(?P<var>[\S]+)\s+"(?P<val>[^"]*)"$"#;
+    lazy_static! {
+        static ref UPS_PATTERN: Regex = Regex::new(RAW_UPS_PATTERN).unwrap();
+        static ref VAR_PATTERN: Regex = Regex::new(RAW_VAR_PATTERN).unwrap();
+    }
+
+    let mut upses: Vec<String> = Vec::new();
+    let mut vars: Vec<UpsVar> = Vec::new();
+
+    // Query UPSes
+    stream.write_all(b"LIST UPS\n").await?;
+    while let Some(line) = stream.lines().next_line().await? {
+        if line.starts_with("BEGIN") {
+            continue;
+        }
+        if line.starts_with("END") {
+            break;
+        }
+        let captures_opt = UPS_PATTERN.captures(&line);
+        match captures_opt {
+            Some(captures) => {
+                let ups = captures["ups"].to_owned();
+                let desc = captures["desc"].to_owned();
+                upses.push(ups.clone());
+                vars.push(UpsVar {ups: ups.clone(), variable: UPS_DESCRIPTION_PSEUDOVAR.to_owned(), value: desc.clone()});
+            },
+            None => {
+                continue;
+            },
+        }
+    }
+
+    // Query VARs
+    for ups in upses {
+        stream.write_all(format!("LIST VAR {}\n", ups).as_bytes()).await?;
+        while let Some(line) = stream.lines().next_line().await? {
+            if line.starts_with("BEGIN") {
+                continue;
+            }
+            if line.starts_with("END") {
+                break;
+            }
+            let captures_opt = VAR_PATTERN.captures(&line);
+            match captures_opt {
+                Some(captures) => {
+                    let ups = captures["ups"].to_owned();
+                    let var = captures["var"].to_owned();
+                    let val = captures["val"].to_owned();
+                    vars.push(UpsVar {ups: ups, variable: var, value: val});
+                },
+                None => {
+                    continue;
+                },
+            }
+        }
+    }
+
+    Ok(vars)
+}
+
+fn build_openmetrics_content(vars: Vec<UpsVar>) -> String {
+    // TODO
+
+    let mut content: String = String::new();
+    for var in vars {
+        content.push_str(&format!("{} {} \"{}\"\n", var.ups, var.variable, var.value));
+    }
+    content
 }
