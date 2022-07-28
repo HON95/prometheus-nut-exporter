8d64517b4cefb6a7c37f72e31ae83f2e61bfc6b2
HON95 <hon@hon.one>
2022-07-28 04:13:10 +0200

    Change default log level to INFO

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,21 @@ mod metrics;
 mod nut_client;
 mod openmetrics_builder;
 
+// Set env var RUST_LOG to override
+pub const LOG_LEVEL_DEFAULT: &str = "info";
+
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    // Setup logger
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(LOG_LEVEL_DEFAULT)).init();
 
+    // Setup config
     let config = config::read_config();
     if config.print_metrics_and_exit {
         metrics::print_metrics();
         return;
     }
 
+    // Run
     http_server::run_server(config).await;
 }
