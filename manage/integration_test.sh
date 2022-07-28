04214ccaa65d0b88b2c4c2efad857d421e4c1a2d
HON95 <hon@hon.one>
2022-07-28 07:35:32 +0200

    Change int test a bit

--- manage/integration_test.sh
+++ manage/integration_test.sh
@@ -2,21 +2,36 @@
 
 set -u -o pipefail
 
+# Start mock
 manage/nut-server-mock.py &>/dev/null &
 mock_pid=$!
 
+# Start exporter
 cargo run &>/dev/null &
 exporter_pid=$!
 
+# Teardown on exit
+function teardown {
+    kill -9 $mock_pid &>/dev/null
+    kill -9 $exporter_pid &>/dev/null
+}
+trap teardown EXIT
+
+# Wait for startup
 sleep 1
 
-scrape_content=$(curl -sSf "http://localhost:9995/nut?target=localhost:3493")
+# Scrape
+scrape_content=$(curl -sSf "http://localhost:99951/nut?target=localhost:3493")
+if [[ $? != 0 ]]; then
+    echo "Failure!"
+    echo "Scrape failed."
+    exit 1
+fi
 
-kill -9 $mock_pid &>/dev/null
-kill -9 $exporter_pid &>/dev/null
-
+# Validate result
 if ! echo $scrape_content | grep 'nut_status{ups="alpha"} 1' &>/dev/null; then
     echo "Failure!"
+    echo "Scraped result contains errors."
     exit 1
 fi
 
